0961936ec2339eae9823ab3933915ba4d6d737aa
Paul Knight <pauljohnknight@gmail.com>
2017-02-25 23:25:39 +0000

    resorted to template

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 (function(){
 'use strict'
 
-var lords = ['dance', 'rings', 'flies'];
-var textline = document.getElementById('textline');
 
-textline.innerHTML = lords;
-var newlords = lords.join(', ');
 
-})(); 
+})();
